0cd4272856fb70327436be16a9fa4fd5bb272f17
NguyenHoangHa9 <112064259+NguyenHoangHa9@users.noreply.github.com>
2023-05-15 11:52:08 +0700

    ex1

---
+++ Atom.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    public class Atom
+    {
+        public int AtomicNumber { get; set; }
+        public string Symbol { get; set; }
+        public string FullName { get; set; }
+        public double AtomicWeight { get; set; }
+
+        public bool Accept()
+        {
+            Console.Write("Nhập số nguyên tử : ");
+            if (!int.TryParse(Console.ReadLine(), out int atomicNumber) || atomicNumber < 1) // 
+            {
+                Console.WriteLine("Invalid input for atomic number. Try again.");
+                return false;
+            }
+            AtomicNumber = atomicNumber;
+
+            Console.Write("Enter symbol: ");
+            string symbol = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(symbol))
+            {
+                Console.WriteLine("Symbol cannot be empty. Try again.");
+                return false;
+            }
+            Symbol = symbol;
+
+            Console.Write("Enter full name: ");
+            string fullName = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(fullName))
+            {
+                Console.WriteLine("Full name cannot be empty. Try again.");
+                return false;
+            }
+            FullName = fullName;
+
+            Console.Write("Enter atomic weight: ");
+            if (!double.TryParse(Console.ReadLine(), out double atomicWeight) || atomicWeight <= 0)
+            {
+                Console.WriteLine("Invalid input for atomic weight. Try again.");
+                return false;
+            }
+            AtomicWeight = atomicWeight;
+
+            return true;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{AtomicNumber,-3} {Symbol,-3} {FullName,-15} {AtomicWeight,-10:F4}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Ex1;
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        const int maxAtoms = 10;
+        Atom[] atoms = new Atom[maxAtoms];
+        int count = 0;
+
+        while (count < maxAtoms)
+        {
+            Console.WriteLine($"Enter information for atom {count + 1}:");
+
+            Atom atom = new Atom();
+            if (atom.Accept())
+            {
+                atoms[count] = atom;
+                count++;
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Atomic information:");
+        Console.WriteLine("Atomic  Symbol Full Name       Atomic Weight");
+        Console.WriteLine("number");
+        Console.WriteLine("------  ------ -------------- -------------");
+
+        foreach (Atom atom in atoms)
+        {
+            if (atom != null)
+            {
+                atom.Display();
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
